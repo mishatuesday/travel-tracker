d67e1f38349a6c595d070354b8b9a1bcf3ca08d2
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-10-24 15:58:34 -0400

    import export

---
+++ src/components/DestinationPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Feature from './Feature';
+import Filter from './Filter';
+import DestinationList from './DestinationList';
+import Form from './Form';
+import Search from './Search';
+
+
+
+
+
+
+
+
+
+export default DestinationPage;
