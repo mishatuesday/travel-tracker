69553892fac559c0bf9be21250e6e3975aac35e1
Kate Galati <kategalati@Kates-MacBook-Air.local>
2022-10-25 08:00:51 -0700

    fetch and list component

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Destination = props => {
+    const {destination} = props
+    
     return (
-        <div>Individual Destinations</div>
+        <div className="destination-list">
+            <p>{destination.name}</p>
+            <img src={destination.image} alt={`Photo of ${destination.name}`}></img>
+        </div>
     )
 }
 
--- src/components/DestinationPage.js
+++ src/components/DestinationPage.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Feature from './Feature';
 import Filter from './Filter';
 import Search from './Search';
 import DestinationList from './DestinationList';
 import AddDestinationForm from './AddDestinationForm';
 
+const destinationUrl = 'http://localhost:3000/destinations/'
+
 function DestinationPage() {
+
+    const [destinations, setDestinations] = useState([])
+
+    useEffect(() => {
+        fetch(destinationUrl)
+        .then(resp => resp.json())
+        .then(setDestinations)
+    },[])
+
     return (
         <div>
             <h2>Destination Page (Route 2)</h2>
             <Search />
             <Filter />
-            <DestinationList />
+            <DestinationList destinations={destinations}/>
             <Feature />
             <AddDestinationForm />
         </div>
