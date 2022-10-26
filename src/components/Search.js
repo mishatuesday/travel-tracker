c0e8bf48eb16f7b7c40ff51dd14ba9ce5aa830e0
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-10-26 11:06:03 -0400

    search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function Search() {
-    return <p>This is Search</p>
+function Search({searchTerm, setSearchTerm}) {
+    
+    return (
+    <div className="searchbar">
+      <label htmlFor="search">Search Destinations:</label>
+      <input
+        type="text"
+        id="search"
+        placeholder="Type a name to search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+    )
+
+   
 }
 
 
