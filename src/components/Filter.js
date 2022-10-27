2d3e071e37132106fe49cd6f31c766fbca3c87d0
Misha Tuesday <mishatuesday@gmail.com>
2022-10-27 13:23:56 -0400

    filter working with state for checkboxes

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,36 +1,24 @@
 import React from 'react';
+import Checkbox from './Checkbox'
 // (checkedAmenities.includes(amenity)) ? "checked='true'" : "checked='false'"
 function Filter({checkedAmenities, setCheckedAmenities, amenitiesFilter, setAmenitiesFilter}) { // no longer need amenitiesFilter and setAmenitiesFilter once checkedAmenities is working
     // const [checkedAmenities, setCheckedAmenities] = useState([]) // this should live in DestinationPage
     // we'll use checkedAmenities.includes() to see if it's checked...
     const amenities = ["Beach", "5-Star Restaurants", "Nightlife", "Hiking", "Museums", "Zip Line"]
-
-    function handleCheckChange(amenity, checked) {
-        console.log("filter checkedAmenities", checkedAmenities)
-        if (checked) {
-            //add amenity to checkedAmenities
-            setCheckedAmenities([...checkedAmenities, amenity])
-        } else {
-            //remove amenity from checkedAmenities
-            const newCheckedAmenities = checkedAmenities.filter(checked => checked !== amenity)
-            setCheckedAmenities(newCheckedAmenities)
-        }
-    }
+    
 
 
     return (
         <div>
             <p>filter by amenities:</p>
             {
-                amenities.map(amenity => {
+                amenities.map((amenity, index) => {
                     return (
-                        <label>
-                            <input key={amenity} type="checkbox" name="filter" id="filter" value={amenity} {...checkedAmenities.includes(amenity) ? "checked='true'" : "checked='false'"} onChange={e => handleCheckChange(amenity, e.target.checked)}/>
-                            {amenity}
-                        </label>
+                        <Checkbox key={index} amenity={amenity} checkedAmenities={checkedAmenities} setCheckedAmenities={setCheckedAmenities} />
                     )
                 })
             }
+            {/* <button onClick={() => setCheckedAmenities([])}>clear all</button> */}
         </div>
     )
 }
